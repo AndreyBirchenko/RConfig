ec73f87e642680b1def3b283d03240ae4ad47eab
Andrey Birchenko <62035054+AndreyBirchenko@users.noreply.github.com>
2024-05-26 18:15:28 +0300

    [Common] Remove example

--- Example/UnitScheme.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RConfig.Runtime.Examples
-{
-    public class UnitScheme : RCScheme
-    {
-        public RCType Health;
-        public RCType Name;
-        public RCType OtherData;
-    }
-}
